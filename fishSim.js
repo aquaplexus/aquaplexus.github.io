71b6b9f19e630c9348ed45cbb841780f70aed169
aquaplexus <aquaplexus16@gmail.com>
2016-03-31 14:03:16 +0200

    Update fishSim.js

--- fishSim.js
+++ fishSim.js
@@ -64,7 +64,7 @@ window.onresize = function(event) {
 function mouseOver(){
   xPos=event.clientX;
   yPos=event.clientY;
-  if (expanded==1&&xPos<300){xPos=-100;}
+  if (expanded==1&&xPos<330){xPos=-100;}
 }
 
 function resize(){
